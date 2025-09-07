1d7b7eb4695e7c1f0f4145177ebc6344195ab1d7
Anastasiia <lobash.n@gmail.com>
2025-09-07 17:36:43 +0300

    feat(components): RecipeCard, DropdownField

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,3 +1,13 @@
-export default function RecipeCard() {
-  return <p>LoadMoreBtn</p>;
-}
+import css from './LoadMoreBtn.module.css';
+
+const LoadMoreBtn = ({ onClick }) => {
+  return (
+    <div className={css.wrapper}>
+      <button className={css.button} onClick={onClick}>
+        Load more
+      </button>
+    </div>
+  );
+};
+
+export default LoadMoreBtn;
